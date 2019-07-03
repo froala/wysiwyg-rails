60474345123edfaee3f75a7bd1b0573ac19df245
Javier Julio <JJfutbol@gmail.com>
2019-07-03 13:04:03 -0400

    Remove assets precompile config initializer
    This is unnecessary as an asset gem. As shown in the docs for wysiwyg-rails, these files are required in manifest files that are part of a Rails applications' precompile config. There is no need to also precompile these files too which causes a very large amount of files being generated when running `rails assets:precompile` (an extra file for each due to GZ).

--- lib/wysiwyg-rails/engine.rb
+++ lib/wysiwyg-rails/engine.rb
@@ -1,17 +1,6 @@
 module WYSIWYG
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'froala.assets.precompile', group: :all do |app|
-        app.config.assets.precompile += %W(
-          froala_editor.min.js
-          plugins/*.js
-          languages/*.js
-          froala_editor.min.css
-          froala_style.min.css
-          plugins/*.css
-          themes/*.css
-        )
-      end
     end
   end
 end
